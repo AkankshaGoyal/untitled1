87f58776ff66af9898ab29625190679427fb728d
akanksha.b.goyal <akankshagoyalju2000@gmail.com>
2024-05-16 23:06:39 +0530

    mycommit

---
+++ src/Test5.java
@@ -0,0 +1,36 @@
+interface A{
+    void a();
+    void b();
+    void c();
+    void d();
+}
+abstract class B implements  A{
+    public void c(){
+        System.out.println("i am c");
+    }
+}
+class M extends  B{
+    public void a(){
+        System.out.println("i am b");
+    }
+    public void b(){
+        System.out.println("i am a");
+    }
+    public void d(){
+        System.out.println("i am d");
+    }
+}
+
+
+
+
+
+public class Test5 {
+    public static void main(String args[]){
+        A a = new M();
+        a.a();
+        a.b();
+        a.c();
+        a.d();
+    }
+}
